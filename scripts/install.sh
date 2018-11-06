67f3f253cb921b666ecc6978a0808d9ee8c51b1e
rodriguesprobr <fernando@rodrigues.pro.br>
2018-11-06 14:30:00 -0200

    README changes - script dir

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install git python3-pip python3-dev -y
-cd ~/
-git clone https://github.com/rodriguesprobr/attini_server.git
-sudo mkdir -p /opt/attini
-sudo chown user:user /opt/attini 
-mv attini_server /opt/attini/server
-sudo -H pip3 install /opt/attini/server
